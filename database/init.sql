a5e458bf84b1655fc11988d5ebad73ab6da7f4a5
Peter Kompasz <kompaszbence@gmail.com>
2024-03-22 11:14:09 +0200

    init refactor

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE host;
 
-\c host
+\c host;
 
 CREATE TABLE users (
   ID SERIAL PRIMARY KEY,
@@ -28,43 +28,27 @@ CREATE TABLE covers (
   premium_quarterly REAL,
   premium_yearly REAL,
   rating REAL,
-  capacity INT
-);
-
-CREATE TABLE covers_smart_contract (
-  ID SERIAL PRIMARY KEY,
-  cover_id INT,
-  name VARCHAR(30),
-  role VARCHAR(20),
-  stx_address VARCHAR(30),
-  CONSTRAINT fk_cover FOREIGN KEY(cover_id) REFERENCES covers(id)
-);
-
-CREATE TABLE covers_de_peg (
-  ID SERIAL PRIMARY KEY,
-  cover_id INT,
-  name VARCHAR(30),
-  role VARCHAR(20),
-  stx_address VARCHAR(30),
-  CONSTRAINT fk_cover FOREIGN KEY(cover_id) REFERENCES covers(id)
+  capacity INT,
+  cover_type TEXT check (cover_type in ('smart_contract', 'de-peg')),
+  stx_address VARCHAR(30)
 );
 
 CREATE TABLE created_covers (
   ID SERIAL PRIMARY KEY,
   cover_id INT,
-  CONSTRAINT fk_cover FOREIGN KEY(cover_id) REFERENCES covers(id) 
+  CONSTRAINT fk_cover FOREIGN KEY(cover_id) REFERENCES covers(id)
 );
 
 CREATE TABLE claims (
   ID SERIAL PRIMARY KEY,
   created_cover_id INT,
-  CONSTRAINT fk_created_cover FOREIGN KEY(created_cover_id) REFERENCES created_covers(id) 
+  CONSTRAINT fk_created_cover FOREIGN KEY(created_cover_id) REFERENCES created_covers(id)
 );
 
 CREATE TABLE votes (
   ID SERIAL PRIMARY KEY,
   claim_id INT,
-  CONSTRAINT fk_claim FOREIGN KEY(claim_id) REFERENCES claims(id) 
+  CONSTRAINT fk_claim FOREIGN KEY(claim_id) REFERENCES claims(id)
 );
 
 CREATE TABLE prompts (
@@ -74,3 +58,15 @@ CREATE TABLE prompts (
   response_type TEXT check (response_type in ('rating', 'description'))
 );
 
+INSERT INTO
+  covers (name, cover_type)
+VALUES
+  ('Uniswap V2', 'smart_contract'),
+  ('1inch', 'smart_contract'),
+  ('Yearn Finance', 'smart_contract'),
+  ('Aave v2', 'smart_contract'),
+  ('USDA - Arkadiko', 'de-peg'),
+  ('UWU - UWU Protocol', 'de-peg'),
+  ('DoC - Dollar on Chain', 'de-peg'),
+  ('USDC', 'de-peg'),
+  ('USDT - Tether USDt', 'de-peg');
