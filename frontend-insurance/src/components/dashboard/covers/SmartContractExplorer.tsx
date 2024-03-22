94186c3d75ddd5b04ed36a8be92f3c20b56da958
Peter Kompasz <kompaszbence@gmail.com>
2024-03-22 11:11:57 +0200

    smart contract covers

--- frontend-insurance/src/components/dashboard/covers/SmartContractExplorer.tsx
+++ frontend-insurance/src/components/dashboard/covers/SmartContractExplorer.tsx
@@ -1,7 +1,57 @@
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useEffect, useState } from "react";
+
 import Explorer from "../../reusable/Explorer.tsx";
+import BuySmartContractCover from "./BuySmartContractCover.tsx";
+import { fetchCovers } from "../../../util/backend.ts";
+import type { Cover } from "./Covers.tsx";
 
 const SmartContractExplorer = () => {
-  return <></>;
+  const [open, setOpen] = useState(false);
+  const [covers, setCovers] = useState([]);
+  const [cover, setCover] = useState({} as Cover);
+  const [error, setError] = useState("");
+
+  const openCover = (cover: Cover) => {
+    setOpen(true);
+    setCover(cover);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    fetchCovers()
+      .then((covers) => {
+        setCovers(covers);
+      })
+      .catch((e) => {
+        setError(e.message);
+      });
+  }, []);
+
+  return (
+    <>
+      {error && <>{error}</>}
+      <Explorer covers={covers} openCover={openCover} error={error}></Explorer>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        style={{ minHeight: "50vh" }}
+        fullWidth
+      >
+        <DialogTitle id="alert-dialog-title">{"Add to Cart"}</DialogTitle>
+        <DialogContent>
+          <BuySmartContractCover cover={cover}></BuySmartContractCover>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 };
 
 export default SmartContractExplorer;
