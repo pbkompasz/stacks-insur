13df00cc8795400d9fa7e372116f272526390f3f
Peter Kompasz <kompaszbence@gmail.com>
2024-03-23 19:22:18 +0200

    amm tests

--- contracts/tests/amm.test.ts
+++ contracts/tests/amm.test.ts
@@ -1,5 +1,5 @@
-
 import { describe, expect, it } from "vitest";
+import { Cl } from "@stacks/transactions";
 
 const accounts = simnet.getAccounts();
 const address1 = accounts.get("wallet_1")!;
@@ -9,9 +9,34 @@ const address1 = accounts.get("wallet_1")!;
   https://docs.hiro.so/clarinet/feature-guides/test-contract-with-clarinet-sdk
 */
 
-describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
-    expect(simnet.blockHeight).toBeDefined();
+describe("text fund init", () => {
+  it("init", () => {
+    const liquidity = simnet.getDataVar("amm", "liquidity");
+    expect(liquidity).toBeUint(0);
+
+    const totalCover = simnet.getDataVar("amm", "total_cover");
+    expect(totalCover).toBeUint(0);
+
+    const price = simnet.getDataVar("amm", "price");
+    expect(price).toBeUint(4);
+  });
+
+  it("deposit", () => {
+    simnet.callPublicFn("amm", "deposit", [Cl.uint(10000)], address1);
+
+    const liquidity = simnet.getDataVar("amm", "liquidity");
+    expect(liquidity).toBeUint(10000);
+  });
+
+  it("redeem", () => {
+    simnet.callPublicFn("amm", "deposit", [Cl.uint(20000)], address1);
+    let liquidity = simnet.getDataVar("amm", "liquidity");
+    expect(liquidity).toBeUint(20000);
+
+    simnet.callPublicFn("amm", "redeem", [Cl.uint(10000)], address1);
+
+    liquidity = simnet.getDataVar("amm", "liquidity");
+    expect(liquidity).toBeUint(10000);
   });
 
   // it("shows an example", () => {
