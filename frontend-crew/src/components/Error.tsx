e70009705b3db0e450b830c0398cc8a4de7e2942
Peter Kompasz <kompaszbence@gmail.com>
2024-03-25 01:44:51 +0200

    error page

---
+++ frontend-crew/src/components/Error.tsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+const Error404 = () => {
+  return (
+    <>
+      <h1>Error</h1>
+      <Button href="/">Go back</Button>
+    </>
+  );
+};
+
+export default Error404;
