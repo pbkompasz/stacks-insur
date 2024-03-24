6125aca4dd849eaa09ce46fc01bd56c9693bfcc4
Peter Kompasz <kompaszbence@gmail.com>
2024-03-25 01:29:49 +0200

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 4000
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 4000;
+const db = require("./src/queries");
+const cors = require("cors");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
+app.use(cors());
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
 
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'myuser',
-  host: 'db',
-  database: 'mydatabase',
-  password: 'mypassword',
-  port: 5432,
-})
-
-const initDatabase = () => {
-  pool.query(`CREATE TABLE IF NOT EXISTS test (test VARCHAR(255)); INSERT INTO test VALUES('test') `) 
-}
-initDatabase();
-
-const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-
-
-
+app.get("/covers", db.getCovers);
